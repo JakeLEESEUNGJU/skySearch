21424bc61dbd0eebba85c6ca1ddbf4d3d13c020b
SYG907 <acr907@naver.com>
2018-07-03 15:18:08 +0900

    참조용 controller/dao/service/util homecontroller 미완성

---
+++ src/main/java/com/skysearch/myapp/dao/ShareDao.java
@@ -0,0 +1,51 @@
+package com.skysearch.myapp.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 
+ * 참조용
+ *
+ */
+
+@Repository
+public class ShareDao {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	public Object getList(String sqlMapId, Object dataMap) {
+		Object result = sqlSession.selectList(sqlMapId, dataMap);
+
+		return result;
+	}
+
+	public Object getObject(String sqlMapId, Object dataMap) {
+		Object result = sqlSession.selectOne(sqlMapId, dataMap);
+
+		return result;
+	}
+
+	public Object saveObject(String sqlMapId, Object dataMap) {
+		Integer result = sqlSession.insert((String) sqlMapId, dataMap);
+
+		return result;
+	}
+
+	public Object deleteObject(String sqlMapId, Object dataMap) {
+
+		Integer result = sqlSession.delete(sqlMapId, dataMap);
+		return result;
+	}
+
+	public Object getUpdate(String sqlMapId, Object dataMap) {
+		Integer result = sqlSession.update(sqlMapId, dataMap);
+		return result;
+	}
+
+}
